2e32c69f1a8c1f265bce956fe7a7d7043ab2ba16
Gaurav Sobti <gauravsobti1@gmail.com>
2020-09-23 23:33:25 +0530

    WIP

--- src/DsAlgo/algo/dynamicProgramming/minimumChessMovesProblem.js
+++ src/DsAlgo/algo/dynamicProgramming/minimumChessMovesProblem.js
@@ -7,22 +7,26 @@ function minChessMoves(source, destination) {
   const n = 8;
   let count = 0,
     visited = {};
-  function recursive(x, y, count, visited) {
-    console.log("x==", x, "y===", y, "count===", count);
-    if (visited[`${x},${y}`]) return count;
-    if (x < 1 || x > n || y < 1 || y > n) return count;
+  function recursive(x, y, count) {
+    if (visited[`${x},${y}`]) {
+      console.log("already visited", `${x},${y}`);
+      return Number.POSITIVE_INFINITY;
+    }
+    // console.log("x==", x, "y===", y, "count===", count);
+    if (x < 1 || x > n || y < 1 || y > n) return Number.POSITIVE_INFINITY;
     // We have reached our destination, we can return
     // count = count + 1;
+    visited[`${x},${y}`] = true;
     if (x === destination.x && y === destination.y) return count;
     count = Math.min(
-      recursive(x - 1, y, count + 1),
-      recursive(x + 1, y, count + 1),
-      recursive(x, y - 1, count + 1),
-      recursive(x, y + 1, count + 1),
-      recursive(x + 1, y + 1, count + 1),
-      recursive(x + 1, y - 1, count + 1),
-      recursive(x - 1, y + 1, count + 1),
-      recursive(x - 1, y - 1, count + 1),
+      // recursive(x - 1, y, count + 1),
+      // recursive(x + 1, y, count + 1),
+      // recursive(x, y - 1, count + 1),
+      // recursive(x, y + 1, count + 1),
+      // recursive(x + 1, y + 1, count + 1),
+      // recursive(x + 1, y - 1, count + 1),
+      // recursive(x - 1, y + 1, count + 1),
+      // recursive(x - 1, y - 1, count + 1),
 
       recursive(x - 2, y - 1, count + 1),
       recursive(x - 1, y - 2, count + 1),
@@ -38,10 +42,13 @@ function minChessMoves(source, destination) {
     );
     return count;
   }
-  recursive(source.x, source.y, count, visited);
+  console.log(
+    `result, minChessMoves to reach from ${source.x},${source.y} to ${destination.x}, ${destination.y}   ===`,
+    recursive(source.x, source.y, count)
+  );
 }
 
-const source = new Cell(4, 4);
-const destination = new Cell(7, 4);
+const source = new Cell(2, 4);
+const destination = new Cell(6, 4);
 
 minChessMoves(source, destination);
