f6e18b8b30b83dee4a238509875c7524ae126acd
Gaurav Sobti <gauravsobti1@gmail.com>
2020-11-30 13:32:35 +0530

    added binary heap with insert and extract max

---
+++ src/DsAlgo/ds/heap/MaxBinaryHeap.js
@@ -0,0 +1,124 @@
+function MaxBinaryHeap() {
+  let values = [];
+  function getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  function pop() {
+    const max = values[0];
+    const end = values.pop();
+    // if heap is not empty, then replace the max value
+    // with the last value and bubble down
+    if (values.length !== 0) {
+      values[0] = end;
+      bubbleDown(0);
+    }
+    return max;
+  }
+
+  function bubbleUp(index) {
+    if (index <= 0) return;
+    const current = values[index];
+    const parentIndex = getParentIndex(index);
+    const parent = values[parentIndex];
+    // is current node greater than parent
+    // then swap the value
+    // and repeat the process
+    if (current > parent) {
+      values[parentIndex] = current;
+      values[index] = parent;
+      bubbleUp(parentIndex);
+    }
+  }
+  function getChildrenIndices(parentIndex) {
+    const leftChild = 2 * parentIndex + 1;
+    const rightChild = 2 * parentIndex + 2;
+    return [leftChild, rightChild];
+  }
+
+  function bubbleDown(index) {
+    if (index < 0 || index >= values.length) return;
+    const parent = values[index];
+    // --- this section is to get the max child ---- //
+    const [leftChildIndex, rightChildIndex] = getChildrenIndices(index);
+    const leftChild = values[leftChildIndex] || Number.NEGATIVE_INFINITY;
+    const rightChild = values[rightChildIndex] || Number.NEGATIVE_INFINITY;
+    const maxChild = Math.max(leftChild, rightChild);
+    const maxChildIndex =
+      leftChild === maxChild ? leftChildIndex : rightChildIndex;
+    // --- section ends here ---- //
+
+    // if parent is less than maxChild, we need to swap and then continue bubble down
+    // for the max child
+    if (parent < maxChild) {
+      values[index] = maxChild;
+      values[maxChildIndex] = parent;
+      bubbleDown(maxChildIndex);
+    }
+  }
+
+  function insert(...valuesToInsert) {
+    valuesToInsert.forEach((value) => {
+      values.push(value);
+      bubbleUp(values.length - 1);
+    });
+  }
+
+  function toString() {
+    console.log(JSON.stringify(values));
+  }
+
+  function getValues() {
+    return [...values];
+  }
+
+  return {
+    getParentIndex,
+    pop,
+    insert,
+    toString,
+    getValues,
+  };
+}
+
+// let myHeap = MaxBinaryHeap();
+// const toInsert = [15, 5, 10, 25, 7, 2, 1, 30, 45];
+// myHeap.insert(...toInsert);
+// console.log("heap after inserting these values ->  ", JSON.stringify(toInsert));
+// myHeap.toString();
+// console.log("Popping, value removed === ", myHeap.pop());
+// console.log("Popping, value removed === ", myHeap.pop());
+
+// console.log("heap now: ");
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+// myHeap.pop();
+// myHeap.toString();
+
+// -- checking scenario when we have only one value in the heap --- //
+function testOnlyOneValueScenario() {
+  let myHeap = MaxBinaryHeap();
+  myHeap.insert(5);
+  console.log("does heap contain value 5 at top?", myHeap.getValues()[0] === 5);
+  const value = myHeap.pop();
+  console.log("is popped value 5?", value === 5);
+  console.log(
+    "after popping value, is heap now empty ? ",
+    myHeap.getValues().length === 0
+  );
+}
+
+testOnlyOneValueScenario();
